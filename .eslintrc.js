b4eab55b791b9f08114d54687bbe20082759aea9
cjf <2940237793@qq.com>
2020-12-23 22:35:02 +0800

    登录模块实现

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-unused-vars': 'warn', // 把该条提示信息转换成警告信息
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    'space-before-blocks': 0,
+    'key-spacing': 0
   }
 }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,93 @@
 <template>
-  <div><h1>登录页面</h1></div>
+  <div class="login-container">
+    <van-nav-bar
+    class="app-nav-bar"
+  title="登录 / 注册"
+  left-arrow
+  @click-left="$router.back()"
+/>
+<van-cell-group>
+  <van-field
+    v-model="user.mobile"
+    left-icon="shouji"
+    placeholder="请输入手机号"
+  > <van-icon class="iconfont iconshouji" slot="left-icon"></van-icon>
+  </van-field>
+  <van-field
+    v-model="user.code"
+    clearable
+    left-icon="iconyanzhengma"
+    placeholder="请输入验证码"
+  >
+  <van-icon class="iconfont iconyanzhengma" slot="left-icon"></van-icon>
+  <template #button>
+    <van-button class="sent-btn" size="mini" round>发送验证码</van-button>
+    </template>
+  </van-field>
+</van-cell-group>
+<div class="login-btn-wrap">
+<van-button class="login-btn" type="info" block @click="onLogin">登录</van-button>
+</div>
+  </div>
 </template>
 
 <script>
+import { login } from '@/api/user'
+import { Toast } from 'vant'
 export default {
+  data(){
+    return {
+      user:{
+        mobile:'',
+        code:''
+      }
+    }
+  },
+  created(){
+
+  },
+  methods:{
+    async onLogin(){
+      Toast.loading({
+        message: '登录中...',
+        forbidClick: true,
+        duration:0
+      })
+      try {
+        const res = await login(this.user)
+        console.log(res)
+        Toast.success('登陆成功')
+      } catch (err) {
+        console.log(err)
+        Toast.fail('登录失败')
+      }
+    }
+  }
 
 }
 
 </script>
-<style scoped>
+<style scoped lang='less'>
+.login-container{
+  .sent-btn{
+    width: 76px;
+    height: 23px;
+    background-color: #ededed;
+    .van-button__text{
+      font-size: 11px;
+      color: #666666;
+    }
+  }
+.login-btn-wrap{
+  padding: 26px 16px;
+  .login-btn{
+  color: #6db4fb;
+  border: none;
+  .van-button__text{
+    font-size: 15px;
+    color: #fff;
+  }
+}
+}
+}
 </style>
